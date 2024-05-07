6d7535e4fe52f7215deacb0a468ca276a5de19f7
Pedro Roratto <pedro_roratto@hotmail.com>
2024-05-07 01:31:49 -0300

    First commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="bg-[#FEFEFF]/10">
+        <Head>Home</Head>
+
+        <header class="bg-black text-white text-center py-3">
+            <img src="/images/kbecas_logo.png" alt="Logo Kbeças Pensantes" class="mx-auto">
+        </header>
+
+        <section class="flex flex-col items-center justify-center h-16 py-16">
+            <div class="text-center w-full">
+                <h2 class="text-3xl font-bold">Quem somos?</h2>
+                <p class="mt-4">
+                    Ministério de jovens da
+                    <a href="https://www.instagram.com/1ipidebauru" target="_blank">@1ipidebauru</a>
+                </p>
+                <p>Fique ligado em nossas programações</p>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script setup>
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<style scoped>
+</style>
